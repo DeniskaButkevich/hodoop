b77984e8308f73ff42a59cd6bb2a3371ddaee23f
DeniskaButkevich <deniskabutkevich@gmail.com>
2022-04-12 11:41:31 +0300

    fix: change datanode dir path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-hdfs namenode -format
 
-hdfs --daemon start namenode
-hdfs --daemon start datanode
+#if [ ! -d "/home/hduser/hadoop-3.3.1/data/dfs/datanode" ]; then
+#        hdfs namenode -format
+#fi
+
+start-dfs.sh
+start-yarn.sh
 
 tail -f /dev/null
