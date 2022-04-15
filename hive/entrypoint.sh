5b235b4073fca680384a7a738058284397485c43
DeniskaButkevich <deniskabutkevich@gmail.com>
2022-04-15 15:08:50 +0300

    fix: connection postgres with metastore

--- hive/entrypoint.sh
+++ hive/entrypoint.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
+cmd="$@"
+
 if [ ! -d "/home/hduser/hadoop-3.3.1/data/dfs/datanode" ]; then
-        hdfs namenode -format
+    hdfs namenode -format
 fi
 
-hdfs --daemon start namenode
-echo "namenode started"
-
-hdfs --daemon start datanode
-echo "datanode started"
+  echo "Starting Hadoop name node..."
+  hdfs --daemon start namenode
+  hdfs --daemon start secondarynamenode
+  yarn --daemon start resourcemanager
+  echo "Starting Hadoop data node..."
+  hdfs --daemon start datanode
+  yarn --daemon start nodemanager
 
 hdfs dfs -mkdir /tmp
 hdfs dfs -chmod g+w /tmp
 hdfs dfs -mkdir -p /user/hive/warehouse
 hdfs dfs -chmod g+w /user/hive/warehouse
 
-echo "postgres -initSchema"
+# ---------------------------
+# checking if postgres has risen
+host="db"
+port="5432"
+
+echo "!! Check postgres for available !!"
+#
+#until curl http://"$host":"$port"; do
+#  echo "postgres is unavailable - sleeping"
+#  sleep 1
+#done
+
+echo "postgres is up - executing command"
+# ---------------------------
+
+echo "Configuring Hive..."
 schematool -dbType postgres -initSchema -verbose
-echo "Start metastore service"
 hive --service metastore &
-echo "JDBC Server"
 hiveserver2 &
-
-
 echo "hive started"
-tail -f /dev/null
+
+exec $cmd
